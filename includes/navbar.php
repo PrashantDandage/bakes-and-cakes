92eac93ebc2d41be5d9f7ab9fb493e5e2c331597
rashantdandage <prashantdandage1999@gmail.com>
2026-08-06 15:18:36 +0530

    proper alignment to the customer navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,58 +1,122 @@
-<!-- Navbar Start -->
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+?>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm sticky-top">
 
     <div class="container">
 
-        <a class="navbar-brand fw-bold text-danger" href="home.php">
+        <!-- Logo -->
+
+        <a class="navbar-brand fw-bold fs-2 text-warning" href="home.php">
+
             🍰 Bakes & Cakes
+
         </a>
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar">
+        <!-- Mobile Toggle -->
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
 
             <span class="navbar-toggler-icon"></span>
 
         </button>
 
-        <div class="collapse navbar-collapse" id="navbar">
+        <!-- Menu -->
 
-            <ul class="navbar-nav ms-auto">
+        <div class="collapse navbar-collapse" id="navbarNav">
+
+            <ul class="navbar-nav ms-auto align-items-center">
 
                 <li class="nav-item">
-                    <a class="nav-link" href="home.php">
+                    <a class="nav-link fw-semibold" href="home.php">
                         Home
                     </a>
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link" href="shop.php">
+                    <a class="nav-link fw-semibold" href="shop.php">
                         Shop
                     </a>
                 </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="about.php">
-                        About
-                    </a>
-                </li>
+                <?php if (isset($_SESSION['customer_id'])) { ?>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="contact.php">
-                        Contact
-                    </a>
-                </li>
+                    <li class="nav-item">
+                        <a class="nav-link fw-semibold" href="my-orders.php">
+                            My Orders
+                        </a>
+                    </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="login.php">
-                        Login
-                    </a>
-                </li>
+                    <li class="nav-item">
+                        <a class="nav-link fw-semibold" href="cart.php">
+                            Cart
+                        </a>
+                    </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="register.php">
-                        Register
-                    </a>
-                </li>
+                    <li class="nav-item">
+                        <a class="nav-link fw-semibold" href="dashboard.php">
+                            Dashboard
+                        </a>
+                    </li>
+
+                    <li class="nav-item ms-3">
+
+                        <span class="fw-bold">
+
+                            👋 <?php echo $_SESSION['customer_name']; ?>
+
+                        </span>
+
+                    </li>
+
+                    <li class="nav-item ms-3">
+
+                        <a href="logout.php" class="btn btn-danger rounded-pill px-4">
+
+                            Logout
+
+                        </a>
+
+                    </li>
+
+                <?php } else { ?>
+
+                    <li class="nav-item">
+                        <a class="nav-link fw-semibold" href="about.php">
+                            About
+                        </a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link fw-semibold" href="contact.php">
+                            Contact
+                        </a>
+                    </li>
+
+                    <li class="nav-item ms-3">
+
+                        <a href="login.php" class="btn btn-outline-success rounded-pill px-4">
+
+                            Login
+
+                        </a>
+
+                    </li>
+
+                    <li class="nav-item ms-2">
+
+                        <a href="register.php" class="btn btn-success rounded-pill px-4">
+
+                            Register
+
+                        </a>
+
+                    </li>
+
+                <?php } ?>
 
             </ul>
 
@@ -61,5 +125,3 @@
     </div>
 
 </nav>
-
-<!-- Navbar End -->
